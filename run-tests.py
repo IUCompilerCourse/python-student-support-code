452d1d41651ad26cc7b9aa21f3be005a4d3f7270
Jeremy Siek <jsiek@indiana.edu>
2022-08-30 16:37:17 -0400

    update run-tests

--- run-tests.py
+++ run-tests.py
@@ -1,19 +1,35 @@
 import os
 import compiler
-import interp_Pvar
-import type_check_Pvar
+import interp_Lvar
+import type_check_Lvar
 from utils import run_tests, run_one_test
+from interp_x86.eval_x86 import interp_x86
 
 compiler = compiler.Compiler()
+
+typecheck_Lvar = type_check_Lvar.TypeCheckLvar().type_check
+
+typecheck_dict = {
+    'source': typecheck_Lvar,
+    'remove_complex_operands': typecheck_Lvar,
+}
+interpLvar = interp_Lvar.InterpLvar().interp
+interp_dict = {
+    'remove_complex_operands': interpLvar,
+    'select_instructions': interp_x86,
+    'assign_homes': interp_x86,
+    'patch_instructions': interp_x86,
+}
+
 if False:
-    run_one_test(os.getcwd() + '/tests/var/zero.py', 'var',
-                 compiler, 'var',
-                 type_check_Pvar.TypeCheckPvar().type_check_P,
-                 interp_Pvar.InterpPvar().interp_P,
-                 None)
+    run_one_test(os.getcwd() + '/tests/var/zero.py',
+                 'var',
+                 compiler,
+                 'var',
+                 typecheck_dict,
+                 interp_dict)
 else:
     run_tests('var', compiler, 'var',
-              type_check_Pvar.TypeCheckPvar().type_check_P,
-              interp_Pvar.InterpPvar().interp_P,
-              None)
+              typecheck_dict,
+              interp_dict)
 
